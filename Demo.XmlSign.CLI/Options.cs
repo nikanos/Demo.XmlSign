dd58b130730f0e77fff73b6d26a0ae91962fbffe
Nikanos Polykarpou <nikanos@windowslive.com>
2022-10-10 21:39:04 +0300

    Add project files.

---
+++ Demo.XmlSign.CLI/Options.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Demo.XmlSign.CLI
+{
+    class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Input XML file to be signed.")]
+        public string InputFile { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output Signed XML files")]
+        public string OutputFile { get; set; }
+
+        [Option('c', "certificate", Required = true, HelpText = "Certificate to be used for signing (should have a private key)")]
+        public string Certificate { get; set; }
+
+        [Option('r', "referenceid", Required = true, HelpText = "Reference ID for element to be signed")]
+        public string ReferenceID { get; set; }
+
+        [Option('n', "name", Required = false, HelpText = "Reference ID attribute name")]
+        public string ReferenceIdAttributeName { get; set; }
+
+    }
+}
---
+++ Demo.XmlSign.CLI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Demo.XmlSign.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+        }
+
+        static void RunOptions(Options opts)
+        {
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+        }
+    }
+}
